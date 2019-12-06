29a12b7178309e467f8b664d48069e19164a4295
Stefanie <stefcodes@gmail.com>
2019-12-06 14:55:13 -0800

    add new and show routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,21 @@ class BooksController < ApplicationController
     get '/books' do 
         @books = Book.all 
         erb :'books/index'
+    end
+    
+
+    get '/books/new' do 
+        @users = User.all
+        erb :'books/new'
+    end
+
+
+    get '/books/:id' do 
+        @book = Book.find_by(id: params[:id])
+        if @book
+            erb:'books/show'
+        else 
+            redirect '/books'
+        end 
     end 
 end 
