7364b3efad5a59de77599d1a02d1419a31e4fb2c
Stefanie <stefcodes@gmail.com>
2019-12-07 03:34:50 -0800

    add styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,15 @@ class UsersController < ApplicationController
       end
 
     post '/signup' do
-        @user = User.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"])
-        @user.save
-        session[:user_id] = @user.id
+        if  params[:email].empty? || params[:password].empty?
+
+            erb :'users/new_error'
+        else 
+            @user = User.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"])
+            @user.save
+            session[:user_id] = @user.id
     
-        redirect '/login'
+            redirect '/login'
+        end 
     end
 end 
