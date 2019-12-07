cac0ff3a01e8be6df4b232ec3815a3271a04dcf3
Stefanie <stefcodes@gmail.com>
2019-12-06 17:42:23 -0800

    add more routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,13 +10,38 @@ class BooksController < ApplicationController
         erb :'books/new'
     end
 
+    post '/books' do
+        user = User.find_by(id: params[:user_id])
+        book = user.books.build(params) 
+        if book.save
+            redirect "/books"
+        else
+            redirect "/books/new"
+        end 
+    end 
+
 
     get '/books/:id' do 
         @book = Book.find_by(id: params[:id])
         if @book
-            erb:'books/show'
+            erb :'books/show'
         else 
             redirect '/books'
         end 
+    end
+    
+    get '/books/:id/edit' do 
+        @users = User.all
+        @book = Book.find_by(id: params[:id])
+        erb :'books/edit'
+    end
+    
+    patch '/books/:id' do 
+        @book = Book.find_by(id: params[:id]) 
+        if @book.update(title: params[:title], author: params[:author], genre: params[:genre])
+            redirect "/books/#{@book.id}"
+        else 
+            redirect "/books/#{@book.id}/edit"
+        end
     end 
 end 
