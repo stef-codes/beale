0354bbf6a0af2a05b0d1b96c4f26725177bf0af6
Stefanie <stefcodes@gmail.com>
2019-12-06 19:45:48 -0800

    add login logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,16 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do 
+    def logged_in?
+      !!session[:user_id]
+    end
+    
+    def current_user
+      @user ||= User.find_by(id: session["user_id"]) if logged_in?
+    end 
+  end 
+
  
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
-SessionsController < ApplicationController
+class SessionsController < ApplicationController
+    
+    get '/login' do 
+        erb :'sessions/login'
+    end
+    
+    post '/login' do 
+        user = User.find_by(email: params[:email])
+
+        if user && user.authenticate(params[:password])
+            session["user_id"] = user.id
+            redirect '/books'
+        else 
+            redirect '/login'
+        end 
+
+    end
+    
+    get '/logout' do 
+        session.clear 
+        redirect '/login'
+    end 
 end 
