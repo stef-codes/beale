6f7b19d6aaef795214f6fb84ec14869d76cc2f92
Jonan Scheffler <jonanscheffler@gmail.com>
2019-12-08 21:19:46 -0800

    Validate title an email at the database level
    When you really want to make sure a value is in the new record before
    you save it you usually want to check that it's not null at a database
    level. You still want to use the software validations, but there are
    ways to bypass those validations inadvertently.
    
    If you use `update_attributes` instead of `update` for example your
    model validations will be skipped.
    
    This way you can be certain that your database itself will actually
    throw an error when something is going to sneak in there.

--- db/migrate/20191206194041_create_books.rb
+++ db/migrate/20191206194041_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :author 
       t.string :genre 
       t.integer :user_id
